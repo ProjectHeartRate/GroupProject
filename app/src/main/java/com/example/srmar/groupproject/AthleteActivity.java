12c0a989cd4c18c9631071031d564af4b2506322
srmartell16 <Srmsrm691>
2017-06-27 11:57:57 -0300

    Range is now displyed on the screen. Java Docs Commenting has been added. Pair: Scott and Jenna

--- app/src/main/java/com/example/srmar/groupproject/AthleteActivity.java
+++ app/src/main/java/com/example/srmar/groupproject/AthleteActivity.java
@@ -1,16 +1,22 @@
 package com.example.srmar.groupproject;
 
-/**
- * Created by srmar on 2017-06-08.
- */
-
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+/**
+ * This class presents a user interface with four buttons, one to go back to the previous screen,
+ * one that opens an interface for tracking steps, one that opnes an interface for tracking
+ * heart rate, and one that allows the athlete to set desired ranges for steps and heartrate.
+ * @author Scott Martell, Jenna McNeil
+ */
 public class AthleteActivity extends AppCompatActivity{
+    /**
+     * This method creates the athlete activity user interface.
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,24 +28,39 @@ public class AthleteActivity extends AppCompatActivity{
         final Button athleteHr = (Button) findViewById(R.id.athleteHr);
         final Button athleteRange = (Button) findViewById(R.id.athleteRange);
 
-        //on click change ui
         athleteStep.setOnClickListener(new View.OnClickListener(){
+            /**
+             * Does nothing
+             * @param view sets the ui.
+             */
             public void onClick(View view){
                 //nothing yet
             }
         });
         athleteHr.setOnClickListener(new View.OnClickListener(){
+            /**
+             * Does nothing.
+             * @param view sets the ui.
+             */
             public void onClick(View view){
                 //nothing yet
             }
         });
         athleteRange.setOnClickListener(new View.OnClickListener(){
+            /**
+             * On click opens AthleteRangeActivity ui.
+             * @param view sets the ui.
+             */
             public void onClick(View view){
                 Intent athleteRangeScreen = new Intent(view.getContext(), AthleteRangeActivity.class);
                 startActivityForResult(athleteRangeScreen, 0);
             }
         });
         athleteBack.setOnClickListener(new View.OnClickListener(){
+            /**
+             * On click opens MainActivity ui.
+             * @param view sets the ui.
+             */
             public void onClick(View view){
                 Intent main = new Intent(view.getContext(), MainActivity.class);
                 startActivityForResult(main, 0);
--- app/src/main/java/com/example/srmar/groupproject/MainActivity.java
+++ app/src/main/java/com/example/srmar/groupproject/MainActivity.java
@@ -6,8 +6,16 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+/**
+ * This class presents an interface with two buttons, Coach and Athlete, which opens the UI
+ * for the coach or the athlete, respectively.
+ * @author Scott Martell, Jenna McNeil
+ */
 public class MainActivity extends AppCompatActivity {
-
+    /**
+     * This method creates the main activity user interface.
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,13 +25,22 @@ public class MainActivity extends AppCompatActivity {
         final Button coachButton = (Button) findViewById(R.id.coach);
         final Button athleteButton = (Button) findViewById(R.id.athlete);
 
-        //on click change ui to athlete or coach
+
         coachButton.setOnClickListener(new View.OnClickListener(){
+            /**
+             * On click opens CoachActivity ui.
+             * @param view sets the ui.
+             */
             public void onClick(View view){
-                //nothing yet
+                Intent coachScreen = new Intent(view.getContext(), CoachActivity.class);
+                startActivityForResult(coachScreen, 0);
             }
         });
         athleteButton.setOnClickListener(new View.OnClickListener(){
+            /**
+             * On click opens AthleteActivity ui.
+             * @param view sets the ui.
+             */
             public void onClick(View view){
                 Intent athleteScreen = new Intent(view.getContext(), AthleteActivity.class);
                 startActivityForResult(athleteScreen, 0);
